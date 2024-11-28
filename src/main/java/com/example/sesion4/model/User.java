80367454e0d0167a079653498e4361ef615a4a3a
ItamiDeveloper <carlosedu062010@hotmail.com>
2024-11-27 21:56:23 -0500

    first commit

---
+++ src/main/java/com/example/sesion4/model/User.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sesion4.model;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class User {
+    
+    private String name;
+    private String email;
+    
+    
+    //Metodos get and set
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
